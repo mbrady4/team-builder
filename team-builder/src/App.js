862e13acd49bafba2c846069ff41ccc44571d928
Michael Brady <mikebrady44@gmail.com>
2020-04-28 19:53:47 -0400

    Completed Stretch Assignment

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -6,7 +6,21 @@ function App() {
 
   const [teamMembers, setTeamMembers] = useState([
     { name: '', email: '', role: '' }
-  ], []);
+  ] );
+
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    role: '',
+    key: -1
+  })
+
+  const onValueChange = event => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   // const onValueChange = event => {
   //   console.log(event.target.name);
@@ -16,24 +30,65 @@ function App() {
   //   }])
   // };
 
-  const addTeamMember = event => {
+  const addTeamMember = (event) => {
     event.preventDefault();
-    setTeamMembers(current => [...current, {
-      name: event.target[0].value,
-      email: event.target[1].value,
-      role: event.target[2].value
+    console.log(formValues.key)
+    if (formValues.key != -1) {
+      console.log("modiying")
+      const modifiedMember = {
+        name: formValues.name,
+        email: formValues.email,
+        role: formValues.role
       }
-    ]);
-};
+      let temp = teamMembers;
+      console.log(temp);
+      temp[formValues.key] = modifiedMember;
+      setTeamMembers(temp)
+    }
+    else {
+      console.log('adding')
+      setTeamMembers(current => [...current, {
+        name: formValues.name,
+        email: formValues.email,
+        role: formValues.role
+        }
+      ]);
+    }
+    console.log(teamMembers);
+    resetFormValues();
+  };
 
-  console.log(teamMembers);
+  const resetFormValues = () => {
+    setFormValues({
+      name: '',
+      email: '',
+      role: '',
+      key: -1
+    });
+  }
+
+  const memberToEdit = (person, key) => {
+    setFormValues({
+      name: person.name,
+      email: person.email,
+      role: person.role,
+      key: key
+    });
+  }
+
   return (
     <div className="App">
      {teamMembers.slice(1).map((person, key) => (
-       <h2 key={key}>Teammate {key} is {person.name}. 
-       Their email is {person.email} and they are a {person.role}</h2>
+       <div>
+        <h2 key={key}>Teammate {key} is {person.name}. 
+        Their email is {person.email} and they are a {person.role}</h2>
+        <button onClick={() => memberToEdit(person, key+1)}>Edit</button>
+        </div>
      ))}
-     <FormBuilder onFormSubmit={addTeamMember}/>
+     <FormBuilder values={formValues} 
+                  onValueChange={onValueChange} 
+                  addTeamMember={addTeamMember} 
+      />
     </div>
   );
 }
--- team-builder/src/components/form.js
+++ team-builder/src/components/form.js
@@ -2,18 +2,24 @@ import React from 'react';
 
 const FormBuilder = (props) => {
     return (
-    <form onSubmit={props.onFormSubmit} >
+    <form onSubmit={props.addTeamMember} >
         <input
             placeholder='Enter Name'
             name='name'
+            value={props.values.name}
+            onChange={props.onValueChange}
         />
         <input
             placeholder='Email'
             name='email'
+            value={props.values.email}
+            onChange={props.onValueChange}
         />
         <input
             placeholder='Role'
             name='role'
+            value={props.values.role}
+            onChange={props.onValueChange}
         />
         <input type='submit'/>
     </form>
